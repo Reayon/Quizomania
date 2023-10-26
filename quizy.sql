d2910347ed7f7fd25dbacb3e4e8bdbd0f1a8f44a
RuDziK <Fillord10@gmail.com>
2023-10-26 20:14:25 +0200

    bazka

--- quizy.sql
+++ quizy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Paź 2023, 22:42
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.1.12
+-- Generation Time: Paź 26, 2023 at 08:13 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `quizy`
+-- Database: `quizy`
 --
 
 -- --------------------------------------------------------
@@ -33,7 +33,7 @@ CREATE TABLE `kategorie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `kategorie`
+-- Dumping data for table `kategorie`
 --
 
 INSERT INTO `kategorie` (`ID_kategorii`, `nazwa`) VALUES
@@ -53,19 +53,23 @@ INSERT INTO `kategorie` (`ID_kategorii`, `nazwa`) VALUES
 CREATE TABLE `odpowiedzi` (
   `ID_odpowiedzi` int(11) NOT NULL,
   `ID_pytania` int(11) NOT NULL,
-  `tresc` varchar(255) NOT NULL,
+  `odp` varchar(255) NOT NULL,
   `czy_poprawna` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `odpowiedzi`
+-- Dumping data for table `odpowiedzi`
 --
 
-INSERT INTO `odpowiedzi` (`ID_odpowiedzi`, `ID_pytania`, `tresc`, `czy_poprawna`) VALUES
+INSERT INTO `odpowiedzi` (`ID_odpowiedzi`, `ID_pytania`, `odp`, `czy_poprawna`) VALUES
 (1, 1, 'Europa', 0),
 (2, 1, 'Ameryka Południowa', 0),
 (3, 1, 'Azja', 0),
-(4, 1, 'Afryka', 1);
+(4, 1, 'Afryka', 1),
+(9, 2, 'W Azji.', 0),
+(10, 2, 'W Europie.', 1),
+(11, 2, 'W Afryce.', 0),
+(12, 2, 'W Ameryce Południowej.', 0);
 
 -- --------------------------------------------------------
 
@@ -80,11 +84,12 @@ CREATE TABLE `pytania` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `pytania`
+-- Dumping data for table `pytania`
 --
 
 INSERT INTO `pytania` (`ID_pytania`, `ID_kategorii`, `tresc`) VALUES
-(1, 5, 'Gdzie leży państwo Niger?');
+(1, 5, 'Gdzie leży państwo Niger?'),
+(2, 5, 'Gdzie leży Polska?');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -111,39 +116,39 @@ ALTER TABLE `pytania`
   ADD KEY `ID_kategorii` (`ID_kategorii`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `kategorie`
+-- AUTO_INCREMENT for table `kategorie`
 --
 ALTER TABLE `kategorie`
   MODIFY `ID_kategorii` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT dla tabeli `odpowiedzi`
+-- AUTO_INCREMENT for table `odpowiedzi`
 --
 ALTER TABLE `odpowiedzi`
-  MODIFY `ID_odpowiedzi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_odpowiedzi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT dla tabeli `pytania`
+-- AUTO_INCREMENT for table `pytania`
 --
 ALTER TABLE `pytania`
-  MODIFY `ID_pytania` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_pytania` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `odpowiedzi`
+-- Constraints for table `odpowiedzi`
 --
 ALTER TABLE `odpowiedzi`
   ADD CONSTRAINT `odpowiedzi_ibfk_1` FOREIGN KEY (`ID_pytania`) REFERENCES `pytania` (`ID_pytania`);
 
 --
--- Ograniczenia dla tabeli `pytania`
+-- Constraints for table `pytania`
 --
 ALTER TABLE `pytania`
   ADD CONSTRAINT `pytania_ibfk_1` FOREIGN KEY (`ID_kategorii`) REFERENCES `kategorie` (`ID_kategorii`);
